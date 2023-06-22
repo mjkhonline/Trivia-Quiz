e02bca3dba37d84a2f38a70863d5a304551ce71a
Mohammad Javad Khademian <mjkhonline@live.com>
2023-06-22 23:27:16 +0330

    set favicon and main view

---
+++ src/views/MainView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import CategorySelection from "@/components/CategorySelection.vue"
+</script>
+
+<template>
+  <main class="h-100">
+    <CategorySelection />
+  </main>
+  <footer class="w-screen fixed bottom-0 py-1 bg-white">
+    <div>
+      <p class="text-center">Developed by <a href="https://github.com/mjkhonline">
+        <span class="text-white bg-black px-2 trans hover:px-1 transition-all duration-150">mjkhonline</span>
+      </a></p>
+    </div>
+  </footer>
+</template>
